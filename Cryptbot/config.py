463db7592551875bacaa6dd92ee72e7ace74ea18
Stanislav Ofitserov <stanislavofitserov4@gmail.com>
2023-01-26 00:39:34 +0500

    Add CryptoBot

---
+++ Cryptbot/config.py
@@ -0,0 +1,10 @@
+TOKEN = '5940843258:AAGr29fqINhz7Ako4aG_1CJcvCkD3rOjO9o'
+
+
+keys = {
+    'Биткоин': 'BTC',
+    'Эфириум': 'ETH',
+    'Доллар': 'USD',
+    'Евро': 'EUR',
+    'Рубли': 'RUB',
+}
--- myclass.py
+++ myclass.py
@@ -1,14 +1,4 @@
-class MyClass():
-   def f(self):
-       return 155
-mc2=MyClass()
-print("It's for test too", mc2.f())
 
-if __name__ == "__main__":
-   mc=MyClass()
-   print("It's only for test", mc.f())
 
-from myclass import MyClass
-if __name__ == "__main__":
-   m=MyClass()
-   print("It's really working:",m.f())
+
+
