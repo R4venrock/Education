df6533436995e8f61446a4bf93e1fc322c59d3bb
R4venrock <121261817+R4venrock@users.noreply.github.com>
2023-03-21 15:37:52 +0500

    D4.7

---
+++ News Portal, D4.7, Ofitserov Stanislav/news/forms.py
@@ -0,0 +1,23 @@
+from django import forms
+from .models import Post
+from django.core.exceptions import ValidationError
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = [
+            'author',
+            'category_type',
+            'post_category',
+            'rating',
+            'title',
+            'text',
+        ]
+
+    def clean(self):
+        cleaned_data = super().clean()
+        text = cleaned_data.get('description')
+        title = cleaned_data.get('name')
+
+        return cleaned_data
